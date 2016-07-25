0625ecdecd0375e829f207ba58e21a9852ade2e0
Kevin Simon <simon.kevin@gmx.de>
2016-07-25 16:37:01 +0200

    Initial Commit

---
+++ rpg-oop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpg-oop
+//
+//  Created by Kevin Simon on 25.07.16.
+//  Copyright © 2016 Kevin Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
